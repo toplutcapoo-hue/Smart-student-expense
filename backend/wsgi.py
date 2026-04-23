4f5ad3ae07c07678217f32a8f2be20a9fd392ded
toplutcapoo-hue <toplutcapoo@gmail.com>
2026-04-23 15:33:50 +0300

    Update wsgi.py

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -7,10 +7,16 @@ For more information on this file, see
 https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
 
+
 import os
+import sys
+
+# Add your project directory to the sys.path
+path = '/home/yourusername/Smart-student-expense'
+if path not in sys.path:
+    sys.path.insert(0, path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'backend.settings'
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
-
 application = get_wsgi_application()
